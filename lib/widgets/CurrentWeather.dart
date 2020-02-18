96c7b797354c8c4abce0afe0233d1243dd7bb4bf
Kravchenko, Denys (Contractor) <denys.kravchenko@stryker.com>
2020-02-18 15:31:57 +0200

    Applying default material theme attributes

--- lib/widgets/CurrentWeather.dart
+++ lib/widgets/CurrentWeather.dart
@@ -1,29 +1,31 @@
 import 'package:flutter/material.dart';
 
 class CurrentWeather extends StatelessWidget {
-
   final _paddingValue = 30.0;
 
   @override
-  Widget build(BuildContext context) => Column(
-        children: <Widget>[
-          Text('Kharkiv',
-              style: new TextStyle(color: Colors.white, fontSize: 32.0)),
-          Padding(padding: EdgeInsets.only(top: _paddingValue)),
-          Text('Sunny', style: new TextStyle(color: Colors.white)),
-          Padding(padding: EdgeInsets.only(top: _paddingValue)),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
+  Widget build(BuildContext context) => Card(
+        margin: const EdgeInsets.all(8.0),
+        child: Padding(
+          padding: const EdgeInsets.all(18.0),
+          child: Column(
             children: <Widget>[
-              Image.network('https://openweathermap.org/img/w/04d.png'),
-              Text('2°C',
-                  style: new TextStyle(
-                      color: Colors.white,
-                      fontSize: 52.0,
-                      fontWeight: FontWeight.bold)),
+              Text('Kharkiv', style: new TextStyle(fontSize: 32.0)),
+              Padding(padding: EdgeInsets.only(top: _paddingValue)),
+              Text('Sunny'),
+              Padding(padding: EdgeInsets.only(top: _paddingValue)),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Image.network('https://openweathermap.org/img/w/04d.png'),
+                  Text('2°C',
+                      style: new TextStyle(
+                          fontSize: 52.0, fontWeight: FontWeight.bold)),
+                ],
+              ),
+              Padding(padding: EdgeInsets.only(top: _paddingValue)),
             ],
           ),
-          Padding(padding: EdgeInsets.only(top: _paddingValue)),
-        ],
+        ),
       );
 }
--- lib/widgets/ForecastWeatherListItem.dart
+++ lib/widgets/ForecastWeatherListItem.dart
@@ -1,30 +1,20 @@
 import 'package:flutter/material.dart';
 
 class ForecastWeatherListItem extends StatelessWidget {
-
   @override
-  Widget build(BuildContext context) =>
-      Card(
-        color: Colors.black,
+  Widget build(BuildContext context) => Card(
+        margin: const EdgeInsets.all(8.0),
         child: Padding(
           padding: const EdgeInsets.all(8.0),
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
-              Text('Fabruary 18, 2020',
-                  style:
-                  new TextStyle(color: Colors.white)),
-              Text('11:30',
-                  style:
-                  new TextStyle(color: Colors.white)),
-              Image.network(
-                  'https://openweathermap.org/img/w/04d.png'),
-              Text('2°C',
-                  style:
-                  new TextStyle(color: Colors.white)),
+              Text('Fabruary 18, 2020'),
+              Text('11:30'),
+              Image.network('https://openweathermap.org/img/w/04d.png'),
+              Text('2°C'),
             ],
           ),
         ),
       );
-
 }
