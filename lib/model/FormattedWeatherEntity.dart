e7272e4caeada485ed96e78bd05c826e9ca2b743
Kravchenko, Denys (Contractor) <denys.kravchenko@stryker.com>
2020-02-18 22:54:58 +0200

    Refactoring

--- lib/model/FormattedWeatherEntity.dart
+++ lib/model/FormattedWeatherEntity.dart
@@ -4,19 +4,19 @@ import 'package:weather/weather.dart';
 class FormattedWeatherEntity {
   final Weather _weather;
 
-  String temperature;
-  String weatherDescription;
-  String weatherIconURL;
-  String date;
-  String time;
-  String city='   ';
+  final String temperature;
+  final String weatherDescription;
+  final String weatherIconURL;
+  final String date;
+  final String time;
+  final String city;
 
-  FormattedWeatherEntity(this._weather) {
-    date = new DateFormat.yMMMd().format(_weather.date);
-    time = new DateFormat.Hm().format(_weather.date);
-    weatherIconURL = 'https://openweathermap.org/img/w/${_weather.weatherIcon}.png';
-    temperature = _weather.temperature.celsius.floor().toString();
-    weatherDescription = _weather.weatherMain;
-    city = _weather.areaName;
-  }
+  FormattedWeatherEntity(this._weather)
+      : date = new DateFormat.yMMMd().format(_weather.date),
+        time = new DateFormat.Hm().format(_weather.date),
+        weatherIconURL =
+            'https://openweathermap.org/img/w/${_weather.weatherIcon}.png',
+        temperature = _weather.temperature.celsius.floor().toString(),
+        weatherDescription = _weather.weatherMain,
+        city = _weather.areaName;
 }
